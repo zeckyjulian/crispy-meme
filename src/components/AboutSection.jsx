090333dc3f2128a95497e48c74b653118a461597
zeckyjulian <julianhimawan@gmail.com>
2025-08-19 17:36:51 +0700

    fix about

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,7 +2,7 @@ import { Briefcase, Code, User } from "lucide-react"
 
 export const AboutSection = () => {
     return (
-        <section id="about" className="py-24 px-24 relative">
+        <section id="about" className="py-16 md:py-24 px-6 md:px-24 relative">
             <div className="container mx-auto max-w-5xl">
                 <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
                     About <span className="text-primary"> Me</span>
