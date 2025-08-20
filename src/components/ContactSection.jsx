c5862fa44038e3a915e9c227688be9168288298a
zeckyjulian <julianhimawan@gmail.com>
2025-08-20 12:27:46 +0700

    mail2

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -56,7 +56,7 @@ export const ContactSection = () => {
                                 </div>
                                 <div>
                                     <h4 className="font-medium text-left"> Email</h4>
-                                    <a href="mailto:julianhimawan@gmail.com" target="_blank" className="text-muted-foreground hover:text-primary transition-colors">
+                                    <a href="mailto:julianhimawan@gmail.com" className="text-muted-foreground hover:text-primary transition-colors">
                                         julianhimawan@gmail.com
                                     </a>
                                 </div>
